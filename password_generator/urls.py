dfecdaa4b4ba1d73dfa33f6c049cfa6c6d86c39f
khushboonyman <khushii.me@gmail.com>
2022-04-24 18:46:43 +0200

    only password generator

--- password_generator/urls.py
+++ password_generator/urls.py
@@ -1,4 +1,4 @@
-"""personal_portfolio URL Configuration
+"""password_generator URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
@@ -13,21 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from django.conf.urls.static import static
-from django.conf import settings
-#from portfolio import views
+
+from django.urls import path
 from generator import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    #path('',views.about,name='about'),
-    #path('blog/', include('blog.urls')),
-    path('password/', views.password, name="password"),
     path('',views.home,name='home'),
+    path('about/',views.about,name='about'),
+    path('password/',views.password, name="password"),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-   
--- portfolio/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse as hr
-from .models import Project
-
-def about(request) :
-    projects = Project.objects.all()
-    return render(request,'blog/about.html',{'projects':projects})
-
-
